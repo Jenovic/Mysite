08d4edb6c5bb894099bc03bae0a0b7b38d668b85
Jenovic Lumu <jenovic@forge.uk>
2020-04-13 14:10:42 +0100

    Update homepage hero structure

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -12,93 +12,77 @@ interface Props {
 class Hero extends React.Component<Props> {
   render() {
     return (
-      <section className="hero is-fullheight is-bold hero-home">
+      <section className="hero is-bold hero-home">
         <div className="hero-body">
-          <div className="container">
+          <div className="cols container">
             <div className="bg-circle"></div>
             <div className="sm-circle"></div>
             <div className="bg-open-circle"></div>
             <div className="sm-open-circle"></div>
-            <div className="level middle-content">
-              <div className="level-item animation">
+            <div className="col is-12 is-6-md content">
+              <div className="animation">
                 <HeroAnimation />
               </div>
-              <div className="level-item image">
-                <img src={require('../assets/3D-PNG-HD.png')} />
-              </div>
-            </div>
-
-            <div className="level">
-              <div className="level-item contact-info-icons">
-                <div className="links">
-                  <AnimatedIcon link="" iconName="linkedin-in" />
-                  <AnimatedIcon
-                    className="orange-shadow"
-                    link=""
-                    iconName="github"
-                  />
-                  <AnimatedIcon
-                    className="orange-border"
-                    link=""
-                    iconName="twitter"
-                  />
-                  <AnimatedIcon
-                    className="orange-background"
-                    link=""
-                    iconName="kaggle"
-                  />
+              <div className="level">
+                <div className="level-item contact-info-icons">
+                  <div className="links">
+                    <AnimatedIcon link="" iconName="linkedin-in" />
+                    <AnimatedIcon
+                      className="orange-shadow"
+                      link=""
+                      iconName="github"
+                    />
+                    <AnimatedIcon
+                      className="orange-border"
+                      link=""
+                      iconName="twitter"
+                    />
+                    <AnimatedIcon
+                      className="orange-background"
+                      link=""
+                      iconName="kaggle"
+                    />
+                  </div>
+                  <div className="vl" />
                 </div>
-                <div className="vl" />
-              </div>
 
-              <div className="level-item description">
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry.
-                </p>
-                <a
-                  onClick={() => {
-                    window.scrollTo({
-                      top: 800,
-                      behavior: 'smooth',
-                    });
-                  }}
-                >
-                  <span>
-                    <div className="wrapper">
-                      <div className="circle-btn btn-2">
-                        <span className="icon  explore">
-                          <i className="fas fa-arrow-right" />
-                        </span>
-                        <span className="circle-bg">
-                          <span>
-                            <em />
+                <div className="level-item description">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry.
+                  </p>
+                  <a
+                    onClick={() => {
+                      window.scrollTo({
+                        top: 800,
+                        behavior: 'smooth',
+                      });
+                    }}
+                  >
+                    <span>
+                      <div className="wrapper">
+                        <div className="circle-btn btn-2">
+                          <span className="icon  explore">
+                            <i className="fas fa-arrow-right" />
                           </span>
-                          <span>
-                            <em />
+                          <span className="circle-bg">
+                            <span>
+                              <em />
+                            </span>
+                            <span>
+                              <em />
+                            </span>
                           </span>
-                        </span>
+                        </div>
                       </div>
-                    </div>
-                  </span>
-                  <span className="link-text">Getting started</span>
-                </a>
+                    </span>
+                    <span className="link-text">Getting started</span>
+                  </a>
+                </div>
               </div>
             </div>
-
-            <div className="hero-foot has-text-centered">
-              <a
-                onClick={() => {
-                  window.scrollTo({
-                    top: 800,
-                    behavior: 'smooth',
-                  });
-                }}
-              >
-                <span className="icon has-text-white is-large bounce">
-                  <i className="fas fa-chevron-down fa-2x"></i>
-                </span>
-              </a>
+            <div className="col is-12 is-6-md image-container">
+              <img src={require('../assets/homegif.gif')} />
             </div>
           </div>
         </div>
